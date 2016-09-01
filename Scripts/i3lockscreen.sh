70c15906b6a9277396cb51f8ad2c16b5f7e84cd1
crowvoorhees <blading82@gmail.com>
2016-09-01 18:55:03 -0400

    playing catch up

--- Scripts/i3lockscreen.sh
+++ Scripts/i3lockscreen.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#! /bin/zsh
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-convert $HOME/Pictures/profile.png -scale 25% /tmp/lock-icon.png
+convert $HOME/Pictures/uac_lock.png -scale 25% /tmp/lock-icon.png
 convert /tmp/screen.png /tmp/lock-icon.png -gravity center -composite -matte /tmp/screen.png
 i3lock -u -i /tmp/screen.png
