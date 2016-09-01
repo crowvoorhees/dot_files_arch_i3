8383cbd9558e328b65f4c338f407de3d28bb8529
crowvoorhees <blading82@gmail.com>
2016-09-01 19:12:59 -0400

    updating lock blur script

--- Scripts/i3lockscreen.sh
+++ Scripts/i3lockscreen.sh
@@ -3,4 +3,4 @@ scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 convert $HOME/Pictures/uac_lock.png -scale 25% /tmp/lock-icon.png
 convert /tmp/screen.png /tmp/lock-icon.png -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
+i3lock -u -i /tmp/screen.png -t
